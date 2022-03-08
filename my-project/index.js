e1d6fd62555c60ff6d2ec560d93ae1603f7cab04
Angel Asenjo <angel-antonio.asenjo@inetum.com>
2022-03-08 11:13:49 +0100

    Practica 3

--- my-project/index.js
+++ my-project/index.js
@@ -1,7 +1,9 @@
 import { registerRootComponent } from 'expo';
 
-import App from './Button/App';
 //import App from './Button/App';
+//import App from './modulo2/estado1';
+//import App from './modulo2/estado3';
+import App from './modulo2/estado4';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
